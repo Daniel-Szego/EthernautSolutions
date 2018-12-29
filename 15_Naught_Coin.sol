0e0dbeff32ec4b3141d152846a8a248f66dec54a
Daniel-Szego <szego.daniel@gmail.com>
2018-12-29 01:12:44 +0100

    Naught coin patch

--- 15_Naught_Coin.sol
+++ 15_Naught_Coin.sol
@@ -1,4 +1,24 @@
+pragma solidity ^0.4.24;
 
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
+    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (_a == 0) {
+      return 0;
+    }
+
+    c = _a * _b;
     assert(c / _a == _b);
     return c;
   }
