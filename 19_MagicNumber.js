19d163a8a45e3a9ccb7449940ab2eb47af544a93
Daniel-Szego <szego.daniel@gmail.com>
2018-12-31 16:04:36 +0100

    lvl 49 still not working

---
+++ 19_MagicNumber.js
@@ -0,0 +1,14 @@
+# Account
+var account = "0x5Fb17f13465950ED8726f7ECE8CBA3dFB37566aF";
+
+# Bytecode
+var bytecode = "0x600a600c600039600a6000f3604260805260206080f3";
+
+# Deploy
+web3.eth.sendTransaction({ from: account, data: bytecode }, function(err,res){console.log(res)});
+
+# Get the contract address
+
+# Set the contract address
+await contract.setSolver("0xF4BD6CebfFc7E50753C2C273d0670D91ABa65a3a");
+
